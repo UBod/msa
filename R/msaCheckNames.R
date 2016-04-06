faf12a44b5bc8832550eeeb2a57476b656b4609b
Ulrich Bodenhofer <bodenhofer@bioinf.jku.at>
2016-04-06 12:24:49 +0000

    Added a new function msaCheckNames; version number bumped to 1.3.4
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/msa@115880 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/msaCheckNames.R
@@ -0,0 +1,20 @@
+msaCheckNames <- function(x, replacement=" ", verbose=TRUE)
+{
+    if (!is(x, "MultipleAlignment"))
+        stop("x must be a multiple alignment object")
+
+    out <- x
+
+    pattern <- "[^a-zA-Z0-9,;:.?!\\-\\(\\)\\'\" ]"
+
+    if (length(grep(pattern, rownames(x), perl=TRUE)) > 0)
+    {
+        if (verbose)
+            message("sequence names contain invalid characters")
+
+        rownames(out) <- gsub(pattern, replacement, rownames(x),
+                              perl=TRUE)
+    }
+
+    invisible(out)
+}
